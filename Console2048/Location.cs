107cc45278cb32f60eca6c4d2828720c0013e5d4
MaGe Bao <52250952+BaoMaGe@users.noreply.github.com>
2021-05-11 21:24:30 +0800

    Console2048

---
+++ Console2048/Location.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console2048
+{
+    /// <summary>
+    /// 位置
+    /// </summary>
+    struct Location
+    {
+        /// <summary>
+        /// 行索引
+        /// </summary>
+        public int RIndex { get; set; }
+
+        /// <summary>
+        /// 列索引
+        /// </summary>
+        public int CIndex { get; set; }
+
+        public Location(int r, int c):this()
+        {
+            this.RIndex = r;
+            this.CIndex = c;
+        }
+    }
+}
