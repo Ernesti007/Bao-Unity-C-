5bd0adfc5a4eb98515e05f7f6bf0c0dc180f36ed
BaoMaGe <2980421614@qq.com>
2021-06-13 03:08:51 +0800

    README.md

--- ConsoleApplication4-03彩票生成器/Program.cs
+++ ConsoleApplication4-03彩票生成器/Program.cs
@@ -10,14 +10,22 @@ namespace ConsoleApplication4_03彩票生成器
     {
         static void Main()
         {
-            foreach (var item in BuyTicket())
+            int []my = BuyTicket();
+            foreach (var item in my)
             {
                 Console.WriteLine(item);
             }
+            int[] ran = Randomticket();
+            foreach (var item2 in ran )
+            {
+                Console.WriteLine(item2);
+            }
+            int money=TicketEq(my,ran);
+            Console.WriteLine("你获得了{0}等奖",money);
             Console.ReadLine();
         }
         private static int[] BuyTicket()
-        {
+        {//在控制台中购买彩票的方法
             int[] ticket = new int[7];
             for (int i = 0; i < 6; i++)
             {
@@ -40,30 +48,50 @@ namespace ConsoleApplication4_03彩票生成器
                 }
             }
             Console.WriteLine("请选择第7注蓝色彩票号码：");
-            ticket[6] = int.Parse(Console.ReadLine());
+            for (; true; )
+            {
+                int bluetest = int.Parse(Console.ReadLine());
+                if (bluetest > 0 && bluetest < 17)
+                {
+                    ticket[6] = bluetest;
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("输入有误，请重新输入1~16范围的数：");
+                }
+            }
             return ticket;
         }
         private static int[] Randomticket()
-        {
-         /*   int[] random = new int[7];
+        {//定义随机产生一注彩票的方法
+            int[] random = new int[7];
             Random ran = new Random();
             for (int i = 0; i < 6; i++)
             {
-                int rannumber = ran.Next(1, 34);
-                if (Array.IndexOf(random, rannumber, 0, i) < 0)
-                    random[i] = rannumber;
+                int Rannumber = ran.Next(1, 34);
+                if (Array.IndexOf(random, Rannumber, 0, i) < 0)
+                    random[i] = Rannumber;
                 else
                 {
                     i--;
                 }
             }
-            return random;*/
+            random[6] = ran.Next(1, 17);
+            return random;
         }
         private static int TicketEq(int[] My, int[] Ran)
-        {
+        {//定义两注彩票比较的方法
             int blue = My[6] == Ran[6] ? 1 : 0;
-
             int jiang = 0;
+            for (int i = 0; i < 6; i++)
+            {
+                if (Array.IndexOf(Ran, My[i], 0, 6) != -1)
+                {
+                    jiang++;
+                }
+            }
+            jiang += blue;
             return jiang;
         }
     }
