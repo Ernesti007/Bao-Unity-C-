087bb260749a9fcf06f0a549728138fe3f28fc9a
BaoMaGe <2980421614@qq.com>
2021-06-18 23:01:54 +0800

    README.md

---
+++ ConsoleApplication5-02/Man.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication5_02
+{
+    class Man
+    {
+        private static string hit;
+        private static int hp;
+        public int HP
+        {
+            get
+            {
+                return hp;
+            }
+            set
+            {
+                int hp = value;
+            }
+        }
+        public string Hit
+        {
+            get
+            {
+                return hit;
+            }
+            set
+            {
+                string hit = value;
+            }
+        }
+    }
+}
