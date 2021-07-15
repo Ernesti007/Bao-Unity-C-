a4da2dd077f2d04eacc01e5361ce4c790cc37e08
BaoMaGe <2980421614@qq.com>
2021-07-15 15:51:58 +0800

    README.md

---
+++ Unity-Project-test/BaseScript/BaseScript/Assets/Script/Base01.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/// <summary>
+///
+/// <summary>
+public class Base01 : MonoBehaviour
+{ 
+    [SerializeField]
+    private int a;
+    public void test()
+    {
+        a= 36;
+    }
+}
+
